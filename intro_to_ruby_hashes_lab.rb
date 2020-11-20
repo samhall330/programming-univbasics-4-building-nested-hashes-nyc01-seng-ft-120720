2561dfb0e038d7c42cd62115ff25e3f813bacf5b
Sam Hall <mann.samantha11@gmail.com>
2020-11-20 23:03:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,20 +1,17 @@
 def base_hash
+  
   {:railroads => {}}
+  
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  
   {:railroads => {:pieces => 4, :rent_in_dollars => {}, :names => {}}}
+  
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  
     {:railroads => {
       :pieces => 4,
       :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200},
@@ -24,9 +21,12 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+
+  {:railroads => {
+    :pieces => 4,
+    :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200},
+    :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :shortline_railroad => {}, :b_and_o_railroad => {}}
+  }
+  }
 
 end
