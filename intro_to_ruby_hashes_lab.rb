73b4fc6b0bf13532c826d517bc95e83e969513fe
Sam Hall <mann.samantha11@gmail.com>
2020-11-20 23:03:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,17 @@
 def base_hash
-  
+
   {:railroads => {}}
-  
+
 end
 
 def monopoly_with_second_tier
-  
+
   {:railroads => {:pieces => 4, :rent_in_dollars => {}, :names => {}}}
-  
+
 end
 
 def monopoly_with_third_tier
-  
+
     {:railroads => {
       :pieces => 4,
       :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200},
