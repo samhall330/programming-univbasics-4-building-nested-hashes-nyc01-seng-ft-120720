fe009953f076d02b455fb793d54f985f192f89b0
Sam Hall <mann.samantha11@gmail.com>
2020-11-20 23:05:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,8 +27,8 @@ def monopoly_with_fourth_tier
     :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200},
     :names => {:reading_railroad => {:mortgage_value => 100},
               :pennsylvania_railroad => {:mortgage_value => 200},
-              :shortline_railroad => {:mortgage_value => 400},
-              :b_and_o_railroad => {:mortgage_value => 800}}
+              :shortline_railroad => {:mortgage_value => 800},
+              :b_and_o_railroad => {:mortgage_value => 400}}
   }
   }
 
